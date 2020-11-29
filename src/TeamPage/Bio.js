d84cf320c34cbab842dd4a4e286ee58d9c57c360
tkong233 <tkong@wesleyan.edu>
2020-11-28 21:14:07 -0500

    center team bio cards

--- src/TeamPage/Bio.js
+++ src/TeamPage/Bio.js
@@ -5,7 +5,17 @@ class Bio extends React.Component {
   render () {
     const { name, role, intro } = this.props;
     return (
-      <Card title={role} extra='' style={{ width: '70%', marginTop: '30px', marginBottom: '30px', textAlign:'left' }}>
+      <Card
+        title={role}
+        extra=''
+        style={{
+          width: '70%',
+          marginTop: '30px',
+          marginBottom: '30px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          textAlign:'left' 
+        }}>
         <h2>{name}</h2>
         <p style={{ textAlign: 'left' }}>{intro}</p>
       </Card>
