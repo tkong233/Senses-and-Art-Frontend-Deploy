339da97c31f36ba908fd2b6bf12688a3020ba9c3
tkong233 <tkong@wesleyan.edu>
2021-01-07 10:22:58 -0500

    moved explore button up

--- src/WelcomePage/WelcomePage.js
+++ src/WelcomePage/WelcomePage.js
@@ -24,8 +24,13 @@ class WelcomePage extends React.Component {
       <div className="main">
         <WelcomeImages/>
         <img src='/sa_text.png'
-             style={{marginBottom: '3em', marginTop: '1em', width: '30%'}}
+             style={{marginBottom: '1em', marginTop: '1em', width: '30%'}}
         />
+        <div className="explore-button">
+          <Button id="button" shape="round" size="large" href="/images">
+            Explore
+          </Button>
+        </div>
         <p>
         Though we cannot meet face-to-face to appreciate the arts, we can cultivate a calm, 
         mindful awareness of our senses as a resource.  While we are apart, we can remain 
@@ -33,16 +38,10 @@ class WelcomePage extends React.Component {
         Senses and Art is place to for you in this difficult time.
         </p>
 
-        {/* <img className='image' src='/welcome.png'/> */}
         <ReactPlayer className='video react-player'
                     url='https://youtu.be/EMciAqjmhso'
         />
 
-        <div className="explore-button">
-          <Button id="button" shape="round" size="large" href="/images">
-            Explore
-          </Button>
-        </div>
       </div>
     );
   }
