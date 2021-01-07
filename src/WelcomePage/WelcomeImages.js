180782fe6df0d5fd17889d1b4105a434aec0bc20
tkong233 <tkong@wesleyan.edu>
2021-01-06 22:51:40 -0500

    fix welcome images?

--- src/WelcomePage/WelcomeImages.js
+++ src/WelcomePage/WelcomeImages.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import img1 from './welcomeImages/1.jpeg';
+import img2 from './welcomeImages/2.jpeg';
+import img3 from './welcomeImages/3.jpeg';
+import img4 from './welcomeImages/4.jpeg';
+import img5 from './welcomeImages/5.jpeg';
+import img6 from './welcomeImages/6.jpeg';
+import img7 from './welcomeImages/7.jpeg';
+import img8 from './welcomeImages/8.jpeg';
+import img9 from './welcomeImages/9.jpeg';
+
 
 class WelcomeImages extends React.Component {
   render() {
     const dir = '/welcomeImages';
     return (
       <div id="container">
-        <img src={dir + '/1.jpeg'}/>
+        <img src={img1}/>
+        <img src={img2}/>
+        <img src={img3}/>
+        <img src={img4}/>
+        <img src={img5}/>
+        <img src={img6}/>
+        <img src={img7}/>
+        <img src={img8}/>
+        <img src={img9}/>
+        {/* <img src={dir + '/1.jpeg'}/>
         <img src={dir + '/2.jpeg'}/>
         <img src={dir + '/3.jpeg'}/>
         <img src={dir + '/4.jpeg'}/>
@@ -13,7 +32,7 @@ class WelcomeImages extends React.Component {
         <img src={dir + '/6.jpeg'}/>
         <img src={dir + '/7.jpeg'}/>
         <img src={dir + '/8.jpeg'}/>
-        <img src={dir + '/9.jpeg'}/>
+        <img src={dir + '/9.jpeg'}/> */}
       </div>
     )
   }
